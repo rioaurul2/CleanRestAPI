27ff0533963e90c18079898ec4c1b8f17bdb395a
rioaurul2 <pmarin08@yahoo.com>
2025-05-07 18:50:30 +0300

    Create Entities

---
+++ TutorialDomain/Entities/Address.cs
@@ -0,0 +1,9 @@
+﻿namespace TutorialDomain.Entities
+{
+    public class Address
+    {
+        public string Street { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+    }
+}
