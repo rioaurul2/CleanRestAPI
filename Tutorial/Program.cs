553daf3cb0438e055e53a0e40e7355b9b3349a1e
rioaurul2 <pmarin08@yahoo.com>
2025-05-15 16:36:26 +0300

    Logging to a file and config in appsettings

--- Tutorial/Program.cs
+++ Tutorial/Program.cs
@@ -18,10 +18,9 @@ namespace Tutorial
 
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddApplication();
-            builder.Host.UseSerilog((context, configuration) => configuration
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Information)
-            .WriteTo.Console(outputTemplate: "[{Timestamp:dd-MM HH:mm:ss} {Level:u3}] |{SourceContext}| {NewLine}{Message:lj}{NewLine}{Exception}"));
+            builder.Host.UseSerilog((context, configuration) => 
+            configuration
+                .ReadFrom.Configuration(context.Configuration));
 
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
